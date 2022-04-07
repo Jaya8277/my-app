99779311bbad3df3492c3aa74816efd62061fd6e
Jaya8277 <jayavish423@gmail.com>
2022-04-07 23:54:18 +0530

    day1prblm2II

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import './App.css';
 import './day1r/listkey.css';
-import List1 from './day1r/list';
+// import List1 from './day1r/list';
 import Listkey from './day1r/listkey';
 import Join from './day1r/Joinus';
 import Login from './day1r/Login';
@@ -11,6 +11,7 @@ import Contact from './day1r/Contact';
 import Search from './day1r/Search';
 import Help from './day1r/Help';
 import Home from './day1r/Home';
+import Allbtnprops from './day1r/Allbtnprops';
 
 
 function App() {
@@ -37,9 +38,35 @@ function App() {
         <Download/>
       </div>
 
+<h1> Problem II</h1>
+<div className="P3">
+  <div className='o'>
+    <Allbtnprops id="Join" name="Join US"/>
+  <Allbtnprops id="Setting" name="Setting US"/>
+  </div>
 
-   
+  <div className='o'>
+    <Allbtnprops id="Login" name="Login US"/>
+  <Allbtnprops id="Contact" name="Contact US"/>
+  </div>
+
+ <div className='o'> 
+   <Allbtnprops id="Search" name="Search US"/>
+  <Allbtnprops id="Help" name="Help US"/>
+  </div>
+
+ <div className='o'>
+    <Allbtnprops id="Home" name="Home US"/>
+  <Allbtnprops id="Download" name="Download US"/>
+  </div>
+</div>
+
+
+
     </div>
+
+
+
   );
 }
 
---
+++ src/day1r/Allbtnprops.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Allbtnprops(props) {
+  return (
+    <div className="Btn"> 
+      <button id ={props.id}>{props.name}</button>
+    </div>
+  )
+}
+
+export default Allbtnprops
